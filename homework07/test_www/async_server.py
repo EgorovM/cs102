0f72f9027dbcda510c67f815348a8ce58f76d857
EgorovM <EgorovM@bitbucket.org>
2020-06-06 17:34:28 +0900

    asyncserver asynchat

--- homework07/test_www/async_server.py
+++ homework07/test_www/async_server.py
@@ -71,7 +71,6 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
 
     def collect_incoming_data(self, data):
         self.collected_data.append(data)
-        self.headers = {}
         self.parse_header = False
 
     def found_terminator(self):
@@ -80,17 +79,17 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
     def parse_request(self):
         if not self.parse_header:
             headers = self.parse_headers()
+
+            if headers.get('Host') is None:
+                self.send_error(200)
+                self.handle_close()
+
             self.parse_header = True
-
-            pprint(headers)
-
-            if headers is None:
-                self.send_error(400, "Bad Request")
-
             self.headers = headers
 
             if headers["method"] == "POST":
-                self.send_error(403)
+                self.send_error(405)
+                self.handle_close()
             else:
                 self.handle_request()
         else:
@@ -103,10 +102,12 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
 
         try:
             headers["method"] = data[0].split()[0][2:]
-            headers["uri"] = data[0].split()[1]
+
+            headers["uri"], headers["args"] = self.translate_path(data[0].split()[1])
             headers["protocol"] = data[0].split()[2]
 
-            for key, value in list(map(lambda x: x.split(':', maxsplit=1), data[1:])):
+            for key, value in list(map(
+                                lambda x: x.split(':', maxsplit=1), data[1:])):
                 headers[str(key)] = value
 
             return headers
@@ -133,10 +134,12 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
         if message is None:
             message = short_msg
 
+        body = f'<html><body><h3>{code}</h3><p>{long_msg}</p></body></html>'
         self.send_response(code, message)
         self.send_header("Content-Type", "text/plain")
         self.send_header("Connection", "close")
-        self.send_header("Content-Length", "71\r\n")
+        self.send_header("Content-Length", f"{len(body)}\r\n")
+        self.sock.sendall(body.encode('utf-8'))
         self.end_headers()
 
     def send_header(self, keyword, value):
@@ -146,29 +149,100 @@ class AsyncHTTPRequestHandler(asynchat.async_chat):
         self.sock.sendall(f'{self.headers["protocol"]} {code} {message}\r\n'.encode())
 
     def end_headers(self):
-        self.sock.sendall(b'<html><head><title>Success</title></head><body>Index page</body></html>')
+        self.sock.sendall(b'\r\n')
+        self.headers = {}
+        self.parse_header = False
+        self.collected_data = []
+
+    def get_content_type(self, uri):
+        content_type = 'text/html'
+
+        if os.path.isfile(f".{uri}"):
+            if 'png' in uri:
+                content_type = 'image/png'
+            if 'jpg' in uri or 'jpeg' in uri:
+                content_type = 'image/jpeg'
+            if 'gif' in uri:
+                content_type = 'image/gif'
+            elif 'js' in uri:
+                content_type = 'application/javascript'
+            elif 'css' in uri:
+                content_type = 'text/css'
+
+        return content_type
 
     def do_GET(self):
-        self.body = {}
+        if self.headers['uri'] == '/':
+            self.headers['uri'] = '/index.html'
+
+        if not os.path.exists(f".{self.headers['uri']}"):
+            self.send_error('404')
+            self.handle_close()
+
+        if self.headers['uri'] != '/' and os.path.isdir(f".{self.headers['uri']}"):
+            self.send_error('403')
+            self.handle_close()
+
+        if self.headers['uri'] != '/' and not os.path.isfile(f".{self.headers['uri']}"):
+            self.send_error('404')
+            self.handle_close()
+
+        content_type = self.get_content_type(self.headers['uri'])
+
+        if os.path.isfile(f".{self.headers['uri']}"):
+            body = open(f".{self.headers['uri']}", 'rb').read()
+        else:
+            body = ''
+
         self.send_response(200, 'OK')
         self.send_header("Date", gmtime())
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", "71\r\n")
+        self.send_header("Content-Type", content_type)
+        self.send_header("Server", "127.0.0.1:8000")
+        self.send_header("Content-Length", f"{len(body)}")
         self.end_headers()
+        self.sock.sendall(body)
+
+    def do_HEAD(self):
+        if self.headers['uri'] == '/':
+            self.headers['uri'] = '/index.html'
+
+        if self.headers['uri'] != '/' and os.path.isdir(f".{self.headers['uri']}"):
+            self.send_error('403')
+            self.handle_close()
+
+        if self.headers['uri'] != '/' and not os.path.isfile(f".{self.headers['uri']}"):
+            self.send_error('404')
+            self.handle_close()
+
+        content_type = self.get_content_type(self.headers['uri'])
+
+        if os.path.isfile(f".{self.headers['uri']}"):
+            body = open(f".{self.headers['uri']}", 'rb').read()
+        else:
+            body = ''
+
+        self.send_response(200, 'OK')
+        self.send_header("Content-Length", f"{len(body)}")
+        self.end_headers()
+        self.sock.sendall(body)
 
     def translate_path(self, path):
-        pass
+        uri = path.split('?')[0].replace('%20', ' ')
+        args = {}
 
+        if '?' in path:
+            args = dict([(el.split('=')[0], el[1].split('=')) for el in path.split('?')[1].split('&')])
 
+        return uri, args
 
     responses = {
-        200: ('OK', 'Request fulfilled, document follows'),
-        400: ('Bad Request',
+        '200': ('OK', 'Request fulfilled, document follows'),
+        '400': ('Bad Request',
             'Bad request syntax or unsupported method'),
-        403: ('Forbidden',
+        '403': ('Forbidden',
             'Request forbidden -- authorization will not help'),
-        404: ('Not Found', 'Nothing matches the given URI'),
-        405: ('Method Not Allowed',
+        '404': ('Not Found', 'Nothing matches the given URI'),
+        '405': ('Method Not Allowed',
             'Specified method is invalid for this resource.'),
     }
 
@@ -184,6 +258,7 @@ def parse_args():
     return parser.parse_args()
 
 def run():
+    print('Start async server', f'{args.host}:{args.port}',)
     server = AsyncServer(host=args.host, port=args.port)
     server.serve_forever()
 
